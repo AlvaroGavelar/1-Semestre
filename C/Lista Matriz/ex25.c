4b91e58bafb1cb2d704a53d375268fbd588a3e12
AlvaroG <avelar.alvaro.gabriel@gmail.com>
2026-07-04 14:18:19 -0300

    C

--- C/Lista Matriz/ex25.c
+++ C/Lista Matriz/ex25.c
@@ -10,6 +10,50 @@ Dica para os alunos: Tente resolver este exercício de duas formas:
 1. Utilizando uma variável contadora auxiliar de 0 a 11 para caminhar pelo vetor enquanto
 percorre a matriz com dois laços for.
 */
-
 #include <stdio.h>
+
+void copiaVetorMatriz(int v[], int m[4][3]){
+    int contador = 0;
+    int col, lin;
+
+    for(lin = 0; lin < 3; lin++){
+        for(col = 0; col < 4; col++){
+            m[lin][col] = v[contador];
+            contador++;
+        }
+    }
+
+    printf("\n");
+
+    for(lin = 0; lin < 3; lin++){
+        for(col = 0; col < 4; col++){
+            printf("%d\t", m[lin][col]);
+        }
+        printf("\n");
+    }    
+}
+
+
+int main(){
+    int v[12];
+    int m[4][3];
+    int col, lin, i, erro, contador;
+
+    for(i = 0; i < 12; i++){
+        erro = 0;
+        do{
+            printf("Forneça Um Numero Inteiro >= 0:");
+            scanf("%d", &v[i]);
+
+            if(v[i] < 0){
+                erro = 1;
+                printf("\nErro!!\n");
+            }
+        }while(erro != 0);
+    }
+
+    copiaVetorMatriz(v, m);    
+
+return 0;
+}
 
