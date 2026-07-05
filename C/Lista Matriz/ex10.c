14327ae868115cc99fc30a1505bf21b071a45000
AlvaroG <avelar.alvaro.gabriel@gmail.com>
2026-07-05 15:31:15 -0300

    C

--- C/Lista Matriz/ex10.c
+++ C/Lista Matriz/ex10.c
@@ -1,8 +1,8 @@
 /*
 Escreva um programa que leia valores inteiros para preencher uma matriz M de dimensões 10x20.
 Após a leitura de todos os dados, o programa deve processar a matriz e exibir na tela:
-1. o maior elemento pertencente a cada uma das linhas;
-2. a média aritmética dos elementos de cada uma das colunas;
+1. o maior elemento pertencente a cada uma das linhas; ok
+2. a média aritmética dos elementos de cada uma das colunas; ok
 3. o resultado do produto (multiplicação) de todos os elementos da matriz que sejam diferentes
 de zero;
 4. a posição exata (linha e coluna) de todas as ocorrências de um valor específico, o qual deve
@@ -15,7 +15,7 @@ ser digitado pelo usuário ao final do programa.
 
 int main(){
     int col, erro, lin, valor, maior[LINHA], mA[COLUNA];
-    int m[LINHA][COLUNA];
+    int m[LINHA][COLUNA], produto, pesquisa;
 
     //Preenche Matriz
     for(lin = 0; lin < LINHA; lin++){
@@ -45,9 +45,41 @@ int main(){
             if(m[lin][col] > maior[lin])
                 maior[lin] = m[lin][col];
         }
+        printf("Maior da linha %d: %d\n", lin+1, maior[lin]);
     }   
 
-    //Media Ari
+    //Media Arit
+    for(col = 0; col < COLUNA; col++){
+        mA[col] = 0;
+        for(lin = 0; lin < LINHA; lin++){
+            mA[col] += m[lin][col];
+        }
+        printf("Media Da Coluna %d: %.1f", col, (float)mA[col] / LINHA);
+    }
+
+    produto = 1;
+
+    //Produto de Tudo
+    for(lin = 0; lin < LINHA; lin++){
+        for(col = 0; col < COLUNA; col++){
+            if(m[lin][col] != 0){
+                produto *= m[lin][col];
+            }
+        }
+    }
+
+    printf("\nProduto Da Matriz: %d\n", produto);
+
+    printf("\nEscolha Um Numero Inteiro Para Vasculhar:");
+    scanf("%d", &pesquisa);
+
+    for(lin = 0; lin < LINHA; lin++){
+        for(col = 0; col < COLUNA; col++){
+            if(m[lin][col] == pesquisa){
+                printf("Encontrado na linha %d coluna %d\n", lin, col);
+            }
+        }
+    }
 
     return 0;
 }
